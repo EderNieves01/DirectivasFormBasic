4f4e2acd005378a8379b9aed70b9e805bf88951e
EderNieves01 <edernieves09@gmail.com>
2023-08-29 01:01:48 -0400

    directiva_v-html

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,14 @@
-<!-- Directiva de texto: v-text -->
+<!-- Directiva de html: v-html que podemos agregar contenido html
+desde nuestro script -->
 
 <template>
   <div>
     <h1>Directivas</h1>
     <hr>
-<!-- se agrega directamente a la etiqueta -->
-<!-- <span v-text="title"></span> -->
 
-<!-- y podemos agregarlo afuera, es mas recomendable por si queremos 
-combinar texto -->
-<span >Eder {{ title }}</span>
+    <!-- me crea un h3 con su contenido creado en en script 
+    pero nos deja vulnerables ataques xss, podemos crear script y pasarlos aqui-->
+<div v-html="html"></div>
   </div>
 
 </template>
@@ -18,7 +17,7 @@ combinar texto -->
 
 export default {
   data: () => ({
-    title: "Hola desde vue"
+    html: "<h3> Hola a todos</h3>"
   })
 }
 </script>
