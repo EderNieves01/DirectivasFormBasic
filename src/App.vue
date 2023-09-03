7c6acc41752409b1b40d2124d14391b79b177eab
EderNieves01 <edernieves09@gmail.com>
2023-09-03 11:48:35 -0400

    Directiva_v-if_&&_v-else

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,4 @@
-<!-- Directiva de show: v-show que podemos ocultar y ver elementos -->
+<!-- Directiva de show: v-if and v-else condicionales -->
 
 <template>
   <div>
@@ -7,11 +7,15 @@
 <h3>Tu edad es: {{ edad }}</h3>
 
 <!-- nos permite crear una condicional que si se cumple muestre
-o desaparece el elemento-->
-<h2 v-show="edad >= 18"> Eres mayor de edad</h2>
-<h2 v-show="edad < 18"> Eres menor de edad</h2>
+o desaparece el elemento casi igual que el v-show pero tendremos mas control
+ sobre los elementos, aqui le tenemos un limited a la edad y no puede
+ser menor a cero ya que nadie tiene esa edad ni mayor que 100-->
+<h2 v-if="edad >= 18 && edad > 0 && edad <100"> Eres mayor de edad</h2>
+<h2 v-else-if="edad < 18 && edad > 0 && edad <100"> Eres menor de edad</h2>
+<h2 v-else>Edad invalida</h2>
 
-<button @click="add()">add</button>
+<button @click="add()">Add</button>
+<button @click="dis()">Dis</button>
   </div>
 
 </template>
@@ -25,6 +29,9 @@ export default {
   methods: {
     add(){
     this.edad++;
+  },
+  dis() {
+    this.edad--;
   }
   }
 }
